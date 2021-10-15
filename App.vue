0bda934c8c850f0bd3503f720786b195558adc91
王元_Trump <861316155@qq.com>
2021-10-15 17:40:49 +0800

    uniapp learning...

---
+++ App.vue
@@ -0,0 +1,24 @@
+<script>
+	export default {
+		globalData: {
+			url: 'http://baidu.com'
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		// 捕获 app error
+		onError(err) {
+			console.error(err)
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ components/test-component/test-component.vue
@@ -0,0 +1,66 @@
+<template>
+	<view>
+		<view class="msg" @click.stop="showToast('name')"> 名字：{{msg}}，年龄：{{age}}</view>
+		<view class="msg" @click="showToast('111')"> 英文名字为：{{nameEnglish}}</view>
+		<slot name="content" :nick="nickname"></slot>
+		<slot :user1="user"></slot>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "test-component",
+		data() {
+			return {
+				nickname: "Lynn",
+				user: {
+					"lastName": "bar",
+					"firstName": "foo"
+				}
+			};
+		},
+		props: {
+			msg: {
+				type: String,
+				default: "default msg",
+				required: true
+			},
+			age: {
+				type: Number,
+				default: 22,
+				validator: function(age) {
+					return age > 30
+				}
+			},
+			name: {
+				type: String,
+				default: 'trump'
+			}
+		},
+
+		computed: {
+			nameEnglish: function() {
+				return this.name.toUpperCase()
+			}
+		},
+
+		methods: {
+
+			showToast(msg) {
+				uni.showToast({
+					title: msg,
+					icon: 'none'
+				});
+			},
+			
+		}
+	}
+</script>
+
+<style>
+	.msg {
+		font-size: 32rpx;
+		font-style: italic;
+		margin-top: 20rpx;
+	}
+</style>
