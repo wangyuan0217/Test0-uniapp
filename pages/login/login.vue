be271295630c763726a6e83764c1e76cc83ff820
王元_Trump <861316155@qq.com>
2021-10-24 01:57:40 +0800

    优化

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,42 +1,25 @@
 <template>
-	<view class="parent">
+	<view class="content">
 
-		<view class="content">
-			<view class="top">
-				<u-image width="220rpx" height="220rpx" src="~@/static/logo.png" />
-				<text class="logo-text">Uni-App</text>
-			</view>
-
-			<view class="center" style="margin: 100rpx 60rpx 0 60rpx;">
-				<view class="line-item">
-					<text class="label">手机号:</text>
-					<view class="line-item-input">
-						<u-input class="input" v-model="phone" type="number" placeholder="请输入手机号" maxlength="11" />
-						<u-line color="lightgray" :hair-line="true" />
-					</view>
-				</view>
-
-				<view class="line-item" style="margin-top: 30rpx;">
-					<text class="label">密码:</text>
-					<view class="line-item-input">
-						<u-input class="input1" v-model="password" type="password" placeholder="请输入密码" maxlength="16" />
-						<u-line color="lightgray" :hair-line="true" />
-					</view>
-				</view>
-			</view>
-
-			<view class="extends">
-				<u-checkbox v-model="remeber" class="remeber" labelSize="30rpx"><text
-						style="color: #666666;">记住密码</text></u-checkbox>
-				<text class="forget-pass">忘记密码</text>
-			</view>
-
-			<u-button class="login" type="primary" shape="circle" size="default" @click="login">登录</u-button>
+		<view class="top">
+			<u-image width="220rpx" height="220rpx" src="~@/static/logo.png" />
+			<text class="logo-text">Uni-App</text>
 		</view>
 
-		<view class="bottom">
-			<text class="privateNote">隐私协议</text>
+		<view class="center">
+			<u-field v-model="phone" label="手机号" type="number" maxlength="11" placeholder="请输入手机号" />
+			<u-field v-model="password" label="密码" type="password" maxlength="16" placeholder="请输入密码" />
 		</view>
+
+		<view class="extends">
+			<u-checkbox v-model="remeber" class="remeber" labelSize="30rpx"><text style="color: #666666;">记住密码</text>
+			</u-checkbox>
+			<text class="forget-pass">忘记密码</text>
+		</view>
+
+		<u-button class="login" type="primary" shape="circle" size="default" @click="login">登录</u-button>
+
+		<text class="private-note">隐私协议</text>
 
 	</view>
 </template>
@@ -46,18 +29,19 @@
 		data() {
 			return {
 				phone: "",
-				password: "123",
+				password: "",
 				remeber: false
 			}
 		},
 		methods: {
 			login: function() {
-
+				console.log("phone = " + this.phone)
+				console.log("password = " + this.password)
+				console.log("remeber = " + this.remeber)
 			}
 		}
 	}
 </script>
-
 
 <style>
 	/* #ifndef H5 */
@@ -75,33 +59,17 @@
 		background-color: #FFFFFF;
 	}
 
-	.parent {
+	.content {
 		height: 100%;
 		display: flex;
 		flex-direction: column;
-		justify-content: space-between;
-
-		.content {
-			margin-top: 150rpx;
-		}
-
-		.bottom {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-
-			.privateNote {
-				margin-bottom: 40rpx;
-				font-size: 30rpx;
-				color: #333333;
-			}
-		}
 	}
 
 	.top {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		margin-top: 150rpx;
 
 		.logo-text {
 			margin-top: 30rpx;
@@ -113,36 +81,27 @@
 	.center {
 		display: flex;
 		flex-direction: column;
+		margin: 100rpx 60rpx 0 60rpx;
 
-		.label {
-			width: 120rpx;
-			font-size: 30rpx;
-			color: #333333,
+		// 修改父组件样式
+		/deep/ .u-field {
+			font-size: 28rpx;
+			padding: 20rpx 0rpx;
+			text-align: left;
+			position: relative;
+			color: $u-main-color;
 		}
 
-		.line-item {
-			display: flex;
-			align-items: center;
-
-			.line-item-input {
-				flex: 1;
-
-				.input {
-					padding: 0rpx 50rpx;
-				}
-
-				.input1 {
-					padding: 0rpx 150rpx;
-				}
-			}
+		/deep/ .u-label-text {
+			font-size: 30rpx;
 		}
 	}
 
 	.extends {
-		margin: 30rpx 60rpx 0 60rpx;
 		display: flex;
 		justify-content: space-between;
 		align-items: baseline;
+		margin: 40rpx 60rpx 0 60rpx;
 
 		.remeber {}
 
@@ -156,5 +115,15 @@
 		font-size: 36rpx;
 		font-weight: bold;
 		margin: 100rpx 80rpx 0 80rpx;
+	}
+
+	.private-note {
+		flex-grow: 1;
+		font-size: 30rpx;
+		margin-bottom: 40rpx;
+		color: #333333;
+		display: flex;
+		justify-content: center;
+		align-items: flex-end;
 	}
 </style>
